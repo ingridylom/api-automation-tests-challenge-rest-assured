e949021fc771fce1834fa8e34c6d04fae0d9ed5f
Ingridy L Messa <ingridylom@hotmail.com>
2023-01-29 02:38:41 -0300

    Adiciona o java-dotenv para ler chaves secretas e org.json para criar objetos json

--- src/test/java/BookingTests.java
+++ src/test/java/BookingTests.java
@@ -2,6 +2,7 @@ import Entities.Booking;
 import Entities.BookingDates;
 import Entities.User;
 import com.github.javafaker.Faker;
+import io.github.cdimascio.dotenv.Dotenv;
 import io.restassured.RestAssured;
 import io.restassured.filter.log.ErrorLoggingFilter;
 import io.restassured.filter.log.RequestLoggingFilter;
@@ -9,6 +10,7 @@ import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.json.JSONObject;
 import org.junit.jupiter.api.*;
 
 import java.text.SimpleDateFormat;
@@ -22,50 +24,65 @@ import static org.hamcrest.Matchers.*;
 
 public class BookingTests {
     public static Faker faker;
+    public static String token;
     private static RequestSpecification request;
     private static Booking booking;
-    private static BookingDates bookingDates;
-    private static User user;
 
     @BeforeAll
     public static void Setup(){
         RestAssured.baseURI = "https://restful-booker.herokuapp.com";
         faker = new Faker();
-        user = new User(faker.name().username(),
+        User user = new User(
+                faker.name().username(),
                 faker.name().firstName(),
                 faker.name().lastName(),
                 faker.internet().safeEmailAddress(),
-                faker.internet().password(8,10),
+                faker.internet().password(8, 10),
                 faker.phoneNumber().toString());
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         Date checkin = faker.date().future(90, 1, TimeUnit.DAYS);
         Date checkout = faker.date().future(90, TimeUnit.DAYS, checkin);
-        bookingDates = new BookingDates(dateFormat.format(checkin), dateFormat.format(checkout));
+        BookingDates bookingDates = new BookingDates(dateFormat.format(checkin), dateFormat.format(checkout));
         booking = new Booking(
-                user.getFirstName(),
-                user.getLastName(),
-                (float)faker.number().randomDouble(2, 50, 100000),
-                true,
+            user.getFirstName(),
+            user.getLastName(),
+            (float)faker.number().randomDouble(2, 50, 100000),
+            true,
                 bookingDates,
-                "");
+            "");
         RestAssured.filters(new RequestLoggingFilter(),new ResponseLoggingFilter(), new ErrorLoggingFilter());
-    }
 
-    @BeforeEach
-    void setRequest(){
+        Dotenv dotenv = Dotenv.load();
+        String username = dotenv.get("USUARIO");
+        String password = dotenv.get("SENHA");
+
+
         request = given().config(RestAssured.config().logConfig(logConfig().enableLoggingOfRequestAndResponseIfValidationFails()))
-                .contentType(ContentType.JSON)
-                .auth().basic("admin", "password123");
+            .contentType(ContentType.JSON)
+            .auth().basic(username, password);
+
+        JSONObject tokenAuthRequestBody = new JSONObject()
+            .put("username", username)
+            .put("password", password);
+
+        token = request
+            .when()
+                .body(tokenAuthRequestBody.toString())
+                .post("/auth")
+            .then()
+                .statusCode(200)
+                .extract()
+                .path("token");
     }
 
     @Test
     public void getAllBookingsById_returnOk(){
-            Response response = request
-                                    .when()
-                                        .get("/booking")
-                                    .then()
-                                        .extract()
-                                        .response();
+        Response response = request
+            .when()
+                .get("/booking")
+            .then()
+                .extract()
+                .response();
 
 
         Assertions.assertNotNull(response);
@@ -74,37 +91,32 @@ public class BookingTests {
 
     @Test
     public void  getAllBookingsByUserFirstName_BookingExists_returnOk(){
-                    request
-                        .when()
-                            .queryParam("firstName", "Carol")
-                            .get("/booking")
-                        .then()
-                            .assertThat()
-                            .statusCode(200)
-                            .contentType(ContentType.JSON)
-                        .and()
-                        .body("results", hasSize(greaterThan(0)));
+        request
+            .when()
+                .queryParam("firstName", "Carol")
+                .get("/booking")
+            .then()
+                .assertThat()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+            .and()
+                .body("results", hasSize(greaterThan(0)));
 
     }
 
     @Test
     public void  CreateBooking_WithValidData_returnOk(){
-
-        Booking test = booking;
-        given().config(RestAssured.config().logConfig(logConfig().enableLoggingOfRequestAndResponseIfValidationFails()))
-                    .contentType(ContentType.JSON)
-                        .when()
-                        .body(booking)
-                        .post("/booking")
-                        .then()
-                        .body(matchesJsonSchemaInClasspath("createBookingRequestSchema.json"))
-                        .and()
-                        .assertThat()
-                        .statusCode(200)
-                        .contentType(ContentType.JSON).and().time(lessThan(2000L));
-
-
-
+        request
+            .contentType(ContentType.JSON)
+                .when()
+                    .body(booking)
+                    .post("/booking")
+                .then()
+                    .body(matchesJsonSchemaInClasspath("createBookingRequestSchema.json"))
+                .and()
+                    .assertThat()
+                    .statusCode(200)
+                    .contentType(ContentType.JSON).and().time(lessThan(2000L));
     }
 
 }
